d4bb1ee38e1a6bc0af8e87f82dc91d5fc7455278
kevin-dwi-c <kevindwi116@gmail.com>
2025-03-15 15:24:04 +0700

    123230110_Kuis

---
+++ src/pkg123230110_kuis/Main.java
@@ -0,0 +1,13 @@
+
+package pkg123230110_kuis;
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/pkg123230110_kuis/ReservasiHotel.java
@@ -0,0 +1,6 @@
+
+package pkg123230110_kuis;
+
+public class ReservasiHotel {
+    
+}
